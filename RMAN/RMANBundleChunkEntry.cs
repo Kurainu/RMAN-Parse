6d4295e2595861af1cfac0f53358e90046f3659d
theshiro01 <theshiro01@gmail.com>
2021-12-02 18:31:04 +0100

    Added rman differ

--- RMAN/RMANBundleChunkEntry.cs
+++ RMAN/RMANBundleChunkEntry.cs
@@ -1,4 +1,6 @@
-﻿namespace RMAN_Parse.RMAN
+﻿using System;
+
+namespace RMAN_Parse.RMAN
 {
     class RMANBundleChunkEntry
     {
@@ -9,5 +11,28 @@
         public int CompressedSize { get; set; }
         public int UncompressedSize { get; set; }
         public string ChunkId { get; set; }
+        public override bool Equals(Object obj)
+        {
+            //Check for null and compare run-time types.
+            if ((obj != null) || this.GetType().Equals(obj.GetType()))
+            {
+                RMANBundleChunkEntry chunk = (RMANBundleChunkEntry)obj;
+
+                return chunk.ChunkId == ChunkId
+                       && chunk.CompressedSize == CompressedSize
+                       && chunk.OffsetinFile == OffsetinFile
+                       && chunk.ParentBundleID == ParentBundleID
+                       && chunk.OffsetinFile == OffsetinFile
+                       && chunk.OffsetToChunk == OffsetToChunk;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override int GetHashCode()
+        {
+            return Convert.ToInt32(ChunkId, 16) ^ Convert.ToInt32(ParentBundleID, 16);
+        }
     }
 }
